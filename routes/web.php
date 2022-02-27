cdabc264d8024d4b8bdb7c4852fe8e2218e35405
Mariana Corona <mariana.corona9828@alumnos.udg.mx>
2022-02-27 13:56:35 -0600

    Vistas

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,37 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/tarea', function () {
+    return view('paginas/hola-mundo');
+});
+
+Route::post('/tarea/store', function () {
+    //validacion 
+    //Limpiexa
+    //guardar
+});
+
+Route::post('/tarea/create', function () {
+    return view('paginas/hola-mundo');
+});
+
+Route::get('/hola-mundo', function () {
+    return view('paginas/hola-mundo');
+});
+/*
+Route::get('/grabaciones/{nombre}', function ($nombre) {
+    //dd($nombre); // Detiene la ejecucion
+    return view('paginas.grabaciones', compact('nombre'));
+});
+*/
+
+Route::get('/grabaciones/{nombre}/{ano?}', function ($nombre, $ano = null) {
+    
+    return view('paginas.grabaciones', compact('nombre','ano'));
+        // -> with([
+        //     'nombre' => $nombre, 
+        //     'otra' => 'variable'
+        // ]);
+});
+
